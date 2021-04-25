e079f6bcba3dadad6162c3b97ebace5a37e81d84
Mayur-Apte <75179671+Mayur-Apte@users.noreply.github.com>
2021-04-25 10:41:31 +0100

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,90)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,120)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,190)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,220)
+    
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A",100,390)
+    fill("lightblue")
+    text("Hint: Stores all information !!",100,420)
+
+    
+
+    
+
+}
